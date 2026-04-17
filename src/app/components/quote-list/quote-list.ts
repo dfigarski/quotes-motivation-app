5cf900a82526a7d1003cd46c3b72f9b37d3cd05c
dfigarski <d.figarski96@gmail.com>
2026-04-17 21:06:39 +0200

    qute list

--- src/app/components/quote-list/quote-list.ts
+++ src/app/components/quote-list/quote-list.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuoteCardComponent } from '../quote-card/quote-card';
 
 @Component({
   selector: 'app-quote-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, QuoteCardComponent],
   templateUrl: './quote-list.html',
   styleUrl: './quote-list.scss',
 })
-export class QuoteList {}
+export class QuoteListComponent {
+  @Input() quotes: any[] = [];
+  @Output() toggle = new EventEmitter<number>();
+
+  onToggle(id: number) {
+    this.toggle.emit(id);
+  }
+}
