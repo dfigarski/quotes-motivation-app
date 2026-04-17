72ab2c5d8a1cd5afa490c4c922f28721dab4871b
dfigarski <d.figarski96@gmail.com>
2026-04-17 21:17:42 +0200

    edytowanie app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { QuoteListComponent } from './components/quote-list/quote-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [QuoteListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('quotes-motivation-app');
+  quotes = [
+    {
+      id: 1,
+      content: 'Success is the sum of small efforts.',
+      author: 'Robert Collier',
+      expanded: false,
+    },
+    {
+      id: 2,
+      content: 'Do not wait. The time will never be just right.',
+      author: 'Napoleon Hill',
+      expanded: false,
+    },
+    {
+      id: 3,
+      content: 'Believe you can and you are halfway there.',
+      author: 'Roosevelt',
+      expanded: false,
+    },
+  ];
+
+  toggleQuote(id: number) {
+    this.quotes = this.quotes.map((q) =>
+      q.id === id ? { ...q, expanded: !q.expanded } : q
+    );
+  }
 }
